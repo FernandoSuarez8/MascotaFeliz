946b9bd8275d183e33758f2d8b59ac3617d204ab
JobTR95 <112602062+JobTR95@users.noreply.github.com>
2022-09-14 22:20:56 -0500

    Update HistoriaClinica.cs

--- PracticaVeterinaria.App.Dominio/Entidades/HistoriaClinica.cs
+++ PracticaVeterinaria.App.Dominio/Entidades/HistoriaClinica.cs
@@ -3,8 +3,19 @@ namespace PracticaVeterinaria.App.Dominio
 {
     public class HistoriaClinica
     {
-        public int id{get; set;}
-        public string fechaApertura{get; set;}
+        public int idHistoria{get; set;}
+        [KEY]
+        public int idMascota{get;set;}
+        [FOREIGN KEY]
+        public int idVeterinario{get;set;}
+        public string Temperatura{get;set;}
+        public string Peso{get;set;}
+        public int frecRespiratoria{get;set;}
+        public int frecCardiaca{get;set;}
+        public string estadoAnimo{get;set;}
+        public string fechaVisita{get; set;}
+        public string Recomendaciones{get;set;}
+        public string formulaMedica{get;set;}
         public Mascota mascota{get;set;}
         
     }
