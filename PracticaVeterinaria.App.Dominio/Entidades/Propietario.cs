ceff0611dc02cef146d3e849ff25bef3e20ae849
fernandosuarez8 <dsuarez53@estudiantes.areandina.edu.co>
2022-09-14 23:32:01 -0500

    Actualización Proyecto MF

--- PracticaVeterinaria.App.Dominio/Entidades/Propietario.cs
+++ PracticaVeterinaria.App.Dominio/Entidades/Propietario.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace PracticaVeterinaria.App.Dominio
 {
-    public class Propietario : Persona
+    public class Propietario : Mascota
     {
         public string cedula { get; set; }
         public string direccion { get; set; }
--- PracticaVeterinaria.App.Dominio/Entidades/Veterinario.cs
+++ PracticaVeterinaria.App.Dominio/Entidades/Veterinario.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace PracticaVeterinaria.App.Dominio
 {
- public class Veterinario : Persona
+ public class Veterinario : Propietario
  {
     public string tarjetaProfesional {get; set;}
  }
