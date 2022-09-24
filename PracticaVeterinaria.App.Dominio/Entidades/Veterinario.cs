e088ffc477976c18d6cd7b630fd0d37cd0a59269
fernandosuarez8 <dsuarez53@estudiantes.areandina.edu.co>
2022-09-24 03:33:13 -0500

    Act.Repo.PMF.240922

--- PracticaVeterinaria.App.Dominio/Entidades/Veterinario.cs
+++ PracticaVeterinaria.App.Dominio/Entidades/Veterinario.cs
@@ -6,7 +6,6 @@ namespace PracticaVeterinaria.App.Dominio
  {
  
     public int id {get;set;}
-    
     public string nombre {get;set;}
     public string apellido {get;set;}
     public string direccion {get;set;}
